c5101c3359447beca125a18d05e895649793f6be
Amber Murray <amber.applebee@gmail.com>
2017-03-26 12:05:55 -0700

    adding updated vendor js file

---
+++ vendor/stackoverflow.js
@@ -0,0 +1,14 @@
+console.log('stackoverflow.js')
+
+function busAnimate () {
+  $(document).ready(function() {
+    function busLeft() {
+      $("#flying-bus").animate({left: "-=3500"}, 4000, "swing", busRight);
+    }
+    function busRight() {
+      $("#flying-bus").animate({left: "+=3500"}, 4000, "swing", busLeft);
+    }
+    busRight();
+
+  });
+}
